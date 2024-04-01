a4d4bc3a40531503910141b0e7523504e1787ec7
LBan <hanxl5123@163.com>
2024-04-01 15:37:39 +0800

    添加爬虫，获取歌曲数据

---
+++ scripts/get-song-data.js
@@ -0,0 +1,121 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const moment = require("moment-timezone");
+
+const HttpProxyAgent = require("http-proxy-agent").HttpProxyAgent;
+const HttpsProxyAgent = require("https-proxy-agent").HttpsProxyAgent;
+const mm = require('music-metadata');
+
+async function getDuration(url) {
+  const response = await axios.get(url, { responseType: 'stream' });
+  const metadata = await mm.parseStream(response.data);
+  return Math.ceil(metadata.format.duration);
+}
+
+// 网络代理，如不需要可删除
+const httpAgent = new HttpProxyAgent("http://127.0.0.1:10808");
+const httpsAgent = new HttpsProxyAgent("http://127.0.0.1:10808");
+
+async function getSongData(id) {
+  const songUrl = `https://app.suno.ai/song/${id}/`;
+
+  const { data } = await axios.get(songUrl, {
+    httpAgent,
+    httpsAgent,
+  });
+  const $ = cheerio.load(data);
+
+  // 解析页面元素
+  const name = $("h2.css-1dklj6k").first().text().trim();
+  const type = "song";
+  const description = $("p.css-0").first().text().trim();
+  const dateStr = $("p.css-ln1ikl").first().text().trim();
+  // 不同语言环境下，日期格式可能不同，需要根据实际情况调整
+  console.log("date", dateStr);
+  const releaseDate = moment
+    .tz(dateStr, "MMMM D, YYYY", "America/New_York")
+    .toDate();
+  const year = releaseDate.getFullYear();
+  const label = $("span.css-1eveppl").first().text().trim();
+  const language = "english";
+  const hasLyrics = true;
+  const isTrending = false;
+  const url = `https://cdn1.suno.ai/${id}.mp3`;
+  const duration = await getDuration(url);
+  const image = [
+    {
+      quality: "200x200",
+      url: $("div.css-cl5oj8 img").attr("src"),
+    },
+  ];
+  const downloadUrl = [
+    {
+      quality: "high",
+      url: url,
+    },
+  ];
+
+  // 歌词
+  const lyrics = {
+    copyright: "",
+    snippet: "",
+    lyrics: $("p.css-w5d21i").first().text().trim(),
+  };
+
+  const songData = {
+    name,
+    description,
+    type,
+    year,
+    releaseDate,
+    label,
+    duration,
+    language,
+    hasLyrics,
+    isTrending,
+    url,
+    image,
+    downloadUrl,
+    lyrics,
+  };
+
+  return songData;
+}
+
+async function fetchWebData() {
+  // 连接MongoDB数据库
+  await mongoose.connect("mongodb://localhost:27017/haya", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log("成功连接到MongoDB数据库");
+
+  // 读取待获取文件
+  const songsJsonData = fs.readFileSync("./scripts/data/songsUrl.json");
+  const urlArray = JSON.parse(songsJsonData);
+
+  if (urlArray.length > 0) {
+    for (let i = 0; i < urlArray.length; i++) {
+      const id = urlArray[i];
+      const songData = await getSongData(id);
+      console.log(songData);
+
+      const Lyrics = require("./models/lyrics");
+      const Song = require("./models/song");
+
+      const lyrics  = songData.lyrics;
+      const lyricsData = await Lyrics.create(lyrics);
+      songData.lyricsId = lyricsData._id;
+      songData.lyrics = lyricsData;
+
+      await Song.create(songData);
+
+      console.log(`成功插入歌曲数据 ${songData.name},id: ${id}`);
+    }
+  }
+}
+
+fetchWebData();
