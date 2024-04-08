1eff3d23cdb633f48e0d7daa181052f0dc28e7c6
Siyu Lin <siyul@tamu.edu>
2024-04-07 21:49:33 -0400

    change api base url

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
         domains: ['c.saavncdn.com','static.saavncdn.com','www.jiosaavn.com'],
     },
     env: {
-      API_BASE_URL: "https://hayasaka-64ccjl6qs-musicly-ai.vercel.app/"
+      // API_BASE_URL: "https://hayasaka-64ccjl6qs-musicly-ai.vercel.app"
+      // API_BASE_URL: "http://localhost:3001"
+      API_BASE_URL: "https://hayasaka-xi.vercel.app"
     }
 }
 
@@ -20,7 +22,9 @@ const withPWA = require('next-pwa')({
   module.exports = withPWA({
     reactStrictMode: true,
     env: {
-      API_BASE_URL: "https://hayasaka-64ccjl6qs-musicly-ai.vercel.app/"
+      // API_BASE_URL: "https://hayasaka-64ccjl6qs-musicly-ai.vercel.app"
+      // API_BASE_URL: "http://localhost:3001"
+      API_BASE_URL: "https://hayasaka-xi.vercel.app"
     }
   });
 
