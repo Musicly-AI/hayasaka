3a00bc454d858388bd10671c57ce50704028d8a0
LBan <hanxl5123@163.com>
2024-04-14 09:08:40 +0800

    Fix the error in querying the playlist.

--- src/app/api/playlists/route.js
+++ src/app/api/playlists/route.js
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/utils/dbconnect";
 import SongPlaylist from '@/models/songplaylist';
+import Song from '@/models/song';
+import Artist from '@/models/artist';
+import Album from '@/models/album';
+import Lyric from '@/models/lyric';
 
 // 根据id获取播放列表
 export async function GET(req) {
